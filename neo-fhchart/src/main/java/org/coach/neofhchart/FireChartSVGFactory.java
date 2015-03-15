c78fb2e2de75c3d7b9000769735713ca7f031c7b
john <decker49@gmail.com>
2015-03-15 16:44:31 -0500

    Adding FireChartSVGFactory.

---
+++ neo-fhchart/src/main/java/org/coach/neofhchart/FireChartSVGFactory.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import org.w3c.dom.*;
+import org.apache.batik.dom.svg.SVGDOMImplementation;
+import org.fhaes.fhfilereader.*;
+
+public class FireChartSVGFactory {
+    public static Document buildSVGFromFile(AbstractFireHistoryReader f){
+        return null;
+    };
+}
